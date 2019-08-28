8c342726763b59bd128184a8dae54093dc850521
skompala <35193623+skompala@users.noreply.github.com>
2019-08-28 13:07:07 -0400

    Update OUTPUT.sh

--- src/OUTPUT.sh
+++ src/OUTPUT.sh
@@ -1,3 +1,10 @@
 export PGPASSWORD=postgres
+CSVCNT=`wc -l Listofstores.sql | cut -d " " -f 1`
+echo "CSV COUNT: $CSVCNT"
+
 psql -h localhost -p 5432 -d postgres -U postgres -f Listofstores.sql
-psql -h localhost -p 5432 -d postgres -U postgres -c 'select city,count(*) from justice.leauge group by city'
+CNT=`psql -h localhost -p 5432 -d postgres -U postgres -t -q -c 'select count(*) from justice.leauge;'`
+echo "COUNT: $CNT"
+CNT=${CNT//[[:space:]]/}
+
+echo "count after trim: $CNT"
