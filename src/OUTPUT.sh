9ee9b1489969d7466598194ca555153ff29dfc27
sujith758 <p.sujithk@gmail.com>
2019-08-28 21:43:52 +0530

    Retail Food Stores

--- src/OUTPUT.sh
+++ src/OUTPUT.sh
@@ -1,3 +1,3 @@
 export PGPASSWORD=postgres
-psql -h localhost -p 5432 -d postgres -U postgres -f listofstores.sql
+psql -h localhost -p 5432 -d postgres -U postgres -f Listofstores.sql
 psql -h localhost -p 5432 -d postgres -U postgres -c 'select city,count(*) from justice.leauge group by city'
